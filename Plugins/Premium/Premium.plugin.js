54fd4beccf74415fb2d52f669104fdd9b862cc12
pipipear <65990558+pipipear@users.noreply.github.com>
2021-06-17 11:45:29 -0700

    Create Premium.plugin.js

---
+++ Plugins/Premium/Premium.plugin.js
@@ -0,0 +1,50 @@
+//META{"name":"Premium"}*//
+
+
+class Premium {
+	/* BD stuff */
+	getName        = () => "Premium";
+	getVersion     = () => "0.1.0";
+	getAuthor      = () => "pi";
+	getDescription = () => "become epic hacker and listen along without giving money to spotify";
+
+
+
+	/* poggers hijacking */
+
+	log(m) {
+		console.log(
+			`%c[%c${this.getName()}%c] %c${m}`,
+			'font-weight: bold; color: #118239',
+			'font-weight: bold; color: #1ED760',
+			'font-weight: bold; color: #118239', ''
+		);
+	}
+
+	load() {
+		if (window.realPremiumFunctions) {
+			this.log('global reference already exists');
+		} else {
+			window.realPremiumFunctions = {};
+			window.realPremiumFunctions.ensureSpotifyPremium = BdApi.findModuleByProps('ensureSpotifyPremium').ensureSpotifyPremium;
+			window.realPremiumFunctions.isSpotifyPremium     = BdApi.findModuleByProps('isSpotifyPremium').ensureSpotifyPremium;
+			this.log('created global reference');
+		}
+	}
+
+	start() {
+		BdApi.findModuleByProps('ensureSpotifyPremium').ensureSpotifyPremium = () => {
+			this.log('intercepted ensureSpotifyPremium');
+			return Promise.resolve();
+		}
+		BdApi.findModuleByProps('isSpotifyPremium').isSpotifyPremium = () => {
+			this.log('intercepted isSpotifyPremium');
+			return true
+		}
+	}
+
+	stop() {
+		BdApi.findModuleByProps('ensureSpotifyPremium').ensureSpotifyPremium = window.realPremiumFunctions.ensureSpotifyPremium;
+		BdApi.findModuleByProps('isSpotifyPremium').isSpotifyPremium         = window.realPremiumFunctions.isSpotifyPremium;
+	}
+}
